0b545eb0e32256c97d4cc31f98156ffd27f0f1f3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-18 12:30:44 -0300

    make a primitive form

---
+++ src/app/pets-form/pets-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-form',
+  templateUrl: './pets-form.component.html',
+  styleUrls: ['./pets-form.component.css'],
+})
+export class PetsFormComponent {}
