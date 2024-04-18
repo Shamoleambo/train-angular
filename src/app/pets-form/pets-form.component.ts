26f80484cda863bb9a6ff015eb66e7bee2f2df3c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-18 14:00:54 -0300

    create a event emitter for Pet Form

--- src/app/pets-form/pets-form.component.ts
+++ src/app/pets-form/pets-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pets-form',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./pets-form.component.css'],
 })
 export class PetsFormComponent {
+  @Output() formSubmit = new EventEmitter<{ type: string; content: string }>();
   petOwnerName: string;
+
+  onFormSubmit() {
+    this.formSubmit.emit({ type: 'formSubmit', content: this.petOwnerName });
+  }
 }
